2c4241a07eec8ad0ae3efe1f051e31673c4b2259
Mateus Albiero <mateusalbiero1@gmail.com>
2024-07-04 17:32:05 -0300

    :sparkles:feat: Add rotas do cadastro de medicos e pacientes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('home', 'LoginController@redirecionarLogin');
+
+Route::prefix('login')->group(function () {
+    Route::get('/', 'LoginController@redirecionarLogin');
+    Route::get('entrar', 'LoginController@entrar')->name('login.entrar');
+    Route::get('sair', 'LoginController@sair')->name('login.sair');
+});
+
+Route::prefix('paciente')->group(function () {
+    Route::get('/', 'PacienteController@index')->name('paciente');
+    Route::get('getpaciente', 'PacienteController@getPaciente')->name('paciente.get');
+    Route::get('cadastro/{id}', 'PacienteController@cadastro')->name('paciente.cadastro');
+    Route::get('buscar/{busca?}', 'PacienteController@buscar')->name('paciente.buscar');
+    Route::post('gravar', 'PacienteController@gravar')->name('paciente.gravar');
+});
+
+Route::prefix('medico')->group(function () {
+    Route::get('/', 'MedicoController@index')->name('medico');
+    Route::get('getmedico', 'MedicoController@getMedico')->name('medico.get');
+    Route::get('cadastro/{id}', 'MedicoController@cadastro')->name('medico.cadastro');
+    Route::get('buscar/{busca?}', 'MedicoController@buscar')->name('medico.buscar');
+    Route::post('gravar', 'MedicoController@gravar')->name('medico.gravar');
 });
