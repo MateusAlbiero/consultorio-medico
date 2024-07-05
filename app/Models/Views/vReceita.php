17f24ddeabfbe2b3a5b6a28241bdb8ce492c20c1
Mateus Albiero <mateusalbiero1@gmail.com>
2024-07-05 14:11:30 -0300

    :sparkles:feat: Adicionado view vReceita

---
+++ app/Models/Views/vReceita.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Views;
+
+use Illuminate\Database\Eloquent\Model;
+
+class vReceita extends Model
+{
+    protected $connection = '';
+    protected $table = 'vreceita';
+    public $timestamps = false;
+}
