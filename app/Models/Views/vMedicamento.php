107a4e4731a3b1752f80edc0ca28e8de76fbdf95
Mateus Albiero <mateusalbiero1@gmail.com>
2024-07-05 15:09:03 -0300

    :sparkles:feat: Adicionado view vMedicamento

---
+++ app/Models/Views/vMedicamento.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Views;
+
+use Illuminate\Database\Eloquent\Model;
+
+class vMedicamento extends Model
+{
+    protected $connection = '';
+    protected $table = 'vmedicamento';
+    public $timestamps = false;
+}
