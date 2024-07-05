a2083d4e2ae5762f73e93e4af9c76737bce6f26f
Mateus Albiero <mateusalbiero1@gmail.com>
2024-07-05 15:10:05 -0300

    :sparkles:feat: Criado rotas na API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\MedicamentoController;
+use App\Http\Controllers\MedicoController;
+use App\Http\Controllers\PacienteController;
+use App\Http\Controllers\ReceitaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('pacientes', PacienteController::class);
+Route::apiResource('medicamentos', MedicamentoController::class);
+Route::apiResource('medicos', MedicoController::class);
+Route::apiResource('receitas', ReceitaController::class);
